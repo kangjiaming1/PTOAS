67e3c38f98d44f9aecb25e044b43a5f6543d5b2e
FangRui <fangrui_95@163.com>
2026-05-07 19:09:23 +0800

    feat: fold fpc into acc_store wrappers

--- lib/PTO/Transforms/PTOPlanMemory.cpp
+++ lib/PTO/Transforms/PTOPlanMemory.cpp
@@ -463,8 +463,7 @@ void MemLivenessAnalysis::RecursionIR(Region *region, Liveness live) {
                    pto::TPutAsyncOp, pto::TGetAsyncOp, pto::TPutOp,
                    pto::TGetOp, pto::TNotifyOp, pto::TWaitOp, pto::TTestOp,
                    pto::TBroadcastOp, pto::CommTGatherOp,
-                   pto::CommTScatterOp, pto::TReduceOp,
-                   pto::CubeLoadFracOp, pto::FpLoadOp>(op)) {
+                   pto::CommTScatterOp, pto::TReduceOp>(op)) {
       UpdateOpGenInfo(curOpInfo, llvm::to_vector(op->getOperands()));
       OpKillHandle(curOpInfo, live, op->getBlock());
     } else if (auto gpuLaunchOp = dyn_cast<gpu::LaunchFuncOp>(op)) {
