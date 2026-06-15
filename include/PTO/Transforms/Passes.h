6fececbe3bedd33bc40e80640ab9a3844d0f9b95
Dongyan Chen <chendongyan@isrc.iscas.ac.cn>
2026-06-15 06:45:48 +0000

    Update testcases.

--- include/PTO/Transforms/Passes.h
+++ include/PTO/Transforms/Passes.h
@@ -103,8 +103,8 @@ std::unique_ptr<Pass> createPTOValidateVPTOEmissionIRPass();
 std::unique_ptr<Pass> createExpandTileOpPass();
 std::unique_ptr<Pass> createExpandTileOpPass(const ExpandTileOpOptions &options);
 std::unique_ptr<Pass> createFoldTileBufIntrinsicsPass();
-std::unique_ptr<Pass> createPTOCanonicalizeIRPass();
 std::unique_ptr<Pass> createFoldTileBufIntrinsicsPass(llvm::StringRef foldMode);
+std::unique_ptr<Pass> createPTOCanonicalizeIRPass();
 std::unique_ptr<Pass>
 createPTOInlineLibCallPass(const PTOInlineLibCallOptions &options = {});
 void registerPTOViewToMemrefPass();
